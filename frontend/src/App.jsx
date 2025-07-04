7c7610a975043b53b028e6488fe3e4e8f3841e94
selkhadr <elkhadrisara01@gmail.com>
2025-07-04 23:47:55 +0100

    switch to ecommerce

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import UserLayout from './components/Layout/UserLayout'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserLayout/>} >
+          <Route index element={<Home/>}/>
+        </Route>
+        <Route>{/*admin layout*/}</Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,19 @@
+import Header from '../Common/Header'
+import Footer from '../Common/Footer'
+
+const UserLayout = () => {
+  return (
+    <>
+    {/**Header */}
+    <Header/>
+    <main>
+      
+    </main>
+    {/**Footer */}
+    <Footer/>
+    {/**main content */}
+    </>
+  )
+}
+
+export default UserLayout
