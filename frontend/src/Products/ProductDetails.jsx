24c1731b69a81659ddb567bdd48a69d1dc6d9147
selkhadr <elkhadrisara01@gmail.com>
2025-07-08 13:21:24 +0100

    add product user may want sectin and make it full responsive

--- frontend/src/Products/ProductDetails.jsx
+++ frontend/src/Products/ProductDetails.jsx
@@ -3,6 +3,7 @@ import bgImg0 from '../assets/bg0.jpg';
 import bgImg1 from '../assets/bg1.jpg';
 import bgImg2 from '../assets/bg2.jpg';
 import bgImg3 from '../assets/bg3.jpg';
+import ProductGrid from './ProductGrid';
 import toast from 'react-hot-toast'; // Assuming you are using react-hot-toast for toasts
 
 const selectedProduct = {
@@ -53,6 +54,33 @@ const selectedProduct = {
         },
     ]
 };
+
+const similarProducts = [
+    {
+        _id:1,
+        name:"product 1",
+        price:100,
+        images:[{url: bgImg1}],
+    },
+    {
+        _id:2,
+        name:"product 2",
+        price:200,
+        images:[{url: bgImg2}],
+    },
+    {
+        _id:3,
+        name:"product 3",
+        price:300,
+        images:[{url: bgImg3}],
+    },
+    {
+        _id:4,
+        name:"product 4",
+        price:400,
+        images:[{url: bgImg1}],
+    },
+]
 
 function ProductDetails() {
     const [mainImg, setMainImg] = useState("");
@@ -223,6 +251,12 @@ function ProductDetails() {
                     </button>
                 </div>
             </div>
+                <div className="mt-20">
+                    <h2 className="text-2xl text-center font-medium mb-4">
+                        You may also like
+                    </h2>
+                    <ProductGrid products={similarProducts} />
+                </div>
         </div>
     );
 }
